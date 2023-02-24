7fa6048210064bd1806cd1f877252b5f3b176a18
Ragib Sakib <ragibsakib2002@gmail.com>
2023-02-23 17:09:15 -0800

    Restructured TMGEController

--- src/TMGEController.java
+++ src/TMGEController.java
@@ -1,17 +1,17 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
+import tmge.TMGE;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 
 public class TMGEController extends Application{
 	
-	public static void main(String[] args) {
-		launch(args);
-	}
+	TMGE tmge;
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		tmge = new TMGE();
 		primaryStage.setTitle("TMGE");
 		
 		Button button = new Button("Coolio");
@@ -23,5 +23,8 @@ public class TMGEController extends Application{
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
-
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
