6f28fe1f0c3d873738df380f3508a3361bd15893
Danielle Carlos <dkcarlos@uci.edu>
2023-03-01 19:28:43 -0800

    Finished CrossExplode and SquareExplode stubs

--- src/tile/CrossExplode.java
+++ src/tile/CrossExplode.java
@@ -3,7 +3,7 @@ import grid.Grid;
 import java.util.List;
 import java.util.ArrayList;
 
-public class CrossExplode {
+public class CrossExplode implements iExploder{
     public List<Integer>explode(Grid g, int row, int col)
     {
         return new ArrayList<Integer>();
