cbd877812a62e732e29a0f20359ce26f1b06d636
Ragib Sakib <ragibsakib2002@gmail.com>
2023-03-14 04:53:12 -0700

    Created Null game display

--- src/tmGame/gameScreen/NullGameScreen.java
+++ src/tmGame/gameScreen/NullGameScreen.java
@@ -2,7 +2,11 @@ package tmGame.gameScreen;
 
 import grid.Grid;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 public class NullGameScreen extends JavaFXScreen implements GameScreen {
@@ -15,11 +19,23 @@ public class NullGameScreen extends JavaFXScreen implements GameScreen {
 	@Override
 	public void displayGrid(Grid grid) {
 		// TODO Auto-generated method stub
-		StackPane gridLayout = new StackPane();
-		System.out.println("PLEASE Work");
-		gridLayout.getChildren().add(new Text("This is a Null Game, get out of here :["));
+		GridPane gameBoard = new GridPane();
+
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+
+				Rectangle tile = new Rectangle(50, 50);
+				tile.setFill(Color.BURLYWOOD);
+				tile.setStroke(Color.BLACK);
+
+				Text text = new Text("sweet");
+				text.setFont(Font.font(12));
+				gameBoard.add(new StackPane(tile, text), j, i);
+
+			}
+    }
 		
-		scene.setRoot(gridLayout);
+		scene.setRoot(gameBoard);
 		
 	}
 
