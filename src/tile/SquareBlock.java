0baa39b8894bc0000546b58cdca03066ef2ceba1
Hyejeong-A-Shin <aliceshin409@gmail.com>
2023-03-14 12:39:36 -0700

    Create SquareBlock.java

---
+++ src/tile/SquareBlock.java
@@ -0,0 +1,33 @@
+package tile;
+import java.util.Map;
+import grid.Position;
+
+public class SquareBlock {
+    Map<Position, Tile> block;
+    boolean isFrozen;
+
+    public void rotate(){
+
+    }
+
+    public void shift(){
+
+    }
+
+    public void fall(){
+
+    }
+
+    public void freeze(){
+
+    }
+
+    public boolean isFrozen(){
+        return isFrozen;
+    }
+
+    public Map<Position, Tile> getBlock(){
+        return block;
+    }
+
+}
