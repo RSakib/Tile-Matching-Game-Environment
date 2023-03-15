0e14696933f0375b7fe166f24efdf074e1af3030
Hyejeong-A-Shin <aliceshin409@gmail.com>
2023-03-14 17:31:54 -0700

    Update IFallable.java

--- src/grid/IFallable.java
+++ src/grid/IFallable.java
@@ -1,5 +1,13 @@
 package grid;
+import java.util.Map;
+
+import tile.Tile;
 
 public interface IFallable {
-    
+    public void rotate();
+    public void shift();
+    public void fall();
+    public void freeze();
+    public boolean isFrozen();
+    public Map<Position, Tile> getBlock();
 }
