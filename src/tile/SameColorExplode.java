510d477e0c69bfc12d579a85bd57535a01859ca6
Danielle Carlos <dkcarlos@uci.edu>
2023-03-16 11:18:18 -0700

    Updated SameColorExplode.java
    SameColorExplode now has functional code that returns the list of positions of other tiles that have the same color.

--- src/tile/SameColorExplode.java
+++ src/tile/SameColorExplode.java
@@ -7,17 +7,25 @@ import grid.Position;
 public class SameColorExplode implements IExploder{
     public List<Position>explode(Grid g, int row, int col)
     {
-        //Work in progress
+        //Work in progres
         ArrayList<Position> toExplode = new ArrayList<Position>();
-        toExplode.add(new Position(row, col)); //Base
+        Position targetPosition = new Position(row, col);
+        Tile targetTile = g.tileAt(targetPosition);
+        toExplode.add(targetPosition); //Base
         //Use grid to get the color of the tile which explodes on the entire board
         for(int currRow= 0; currRow < g.getNumRows(); currRow++)
         {
             for(int currCol = 0; currCol < g.getNumCols(); currCol++)
             {
-                //Get tile color
+                //If colors are the same, add to list
+                Position currPosition = new Position(currRow, currCol);
+                Tile currTile = g.tileAt(currPosition);
+                if(targetTile.getColor() == currTile.getColor())
+                {
+                    toExplode.add(currPosition);
+                }
             }
         }
-        return new ArrayList<Position>();
+        return toExplode;
     }
 }
