1b053b69336fd672e061a5296884e38be477bd62
Danielle Carlos <dkcarlos@uci.edu>
2023-03-18 16:00:54 -0700

    Added TimeUp Logic and applyGravity Logic Stub
    Finished TimeUp Logic that needs testing and tweaking. Currently working on applyGravity and Bejeweled Grid

--- src/grid/BejeweledGrid.java
+++ src/grid/BejeweledGrid.java
@@ -18,6 +18,7 @@ public class BejeweledGrid extends Grid{
         new HorizontalMatchingPattern(COLS),
         new VerticalMatchingPattern(ROWS)
     };
+    private final static int TOP_ROW_INDICATOR = -1; //for use in applyGravity to indicate top row
 
     BejeweledGrid(int rows, int cols, IMatcher matcher, IMatchingPattern[] patterns)
     {
@@ -72,8 +73,42 @@ public class BejeweledGrid extends Grid{
 
     @Override
     public void applyGravity() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'applyGravity'");
+        //Go through grid to find empty tiles
+        for(int row = 0; row < getNumRows(); row++)
+        {
+            for(int col = 0; col < getNumCols(); col++)
+            {
+                Position currPosition = new Position(row, col);
+                Tile currTile = tileAt(currPosition);
+                if(currTile instanceof EmptyTile)
+                {
+                    //Shift everything down by one column
+                    int rowAbove = row -1;
+                    for(int currRowAbove = rowAbove; currRowAbove >= TOP_ROW_INDICATOR; currRowAbove--)
+                    {
+                        if(rowAbove >= 0 && rowAbove < getNumRows())
+                        {
+                            Tile tileAbove = tileAt(new Position(row-1, col));
+                            //Check if empty. If not, set currentTile to tile above
+                            if(!(tileAbove instanceof EmptyTile))
+                            {
+                                //recursively bring down column up to 0
+                                setTile(currPosition, tileAbove);
+                            }
+                        }
+                        else
+                        {
+                            //Create new Tile -- Empty Tile is Top Row
+                        }
+                    }
+                } 
+            }
+        }
+    }
+        //If empty, bring the row above it down 
+        //If top row, generate new tiles
+        //Run matchAt to see if there are any matches
+        //If no matches, end method    
     }
     
 }
