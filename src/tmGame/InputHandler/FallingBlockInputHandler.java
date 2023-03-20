f1c19ec236fd5932c7b012389b9e6538705458ca
Ragib Sakib <ragibsakib2002@gmail.com>
2023-03-19 17:31:56 -0700

    Down Key is now a speed up function
    like real tetris

--- src/tmGame/InputHandler/FallingBlockInputHandler.java
+++ src/tmGame/InputHandler/FallingBlockInputHandler.java
@@ -28,7 +28,7 @@ public class FallingBlockInputHandler extends InputHandlerJFX {
                 ((FallingBlockGrid)game.getGrid()).rotateFaller();
             }
             else if (input == DOWNCODE) {
-                ((FallingBlockGrid)game.getGrid()).shiftFaller(Direction.DOWN);
+                ((FallingBlockGrid)game.getGrid()).moveFallerDown();;
             }
             else {
                 return;
