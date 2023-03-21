4074539e78d9e209fd8afeb7c56e186885f468c9
Ragib Sakib <ragibsakib2002@gmail.com>
2023-03-21 14:24:10 -0700

    Finished Display
    May need to rename tile.Color

--- src/tmGame/gameScreen/BejeweledGameScreen.java
+++ src/tmGame/gameScreen/BejeweledGameScreen.java
@@ -1,13 +1,59 @@
 package tmGame.gameScreen;
 
 import grid.Grid;
+import grid.Position;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Rectangle;
+import tile.Color;
 
 public class BejeweledGameScreen extends GameScreenJFX{
 
     @Override
     public void displayGrid(Grid grid) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'displayGrid'");
+        GridPane gameBoard = new GridPane();
+
+        for (int i = 0; i < grid.getNumRows(); i++) {
+			for (int j = 0; j < grid.getNumCols(); j++) {
+
+				Rectangle tile = new Rectangle(10, 10);
+                
+                switch (grid.tileAt(new Position(i,j)).getColor()) {
+                    case RED:
+                        tile.setFill(javafx.scene.paint.Color.RED);
+                        break;
+                    case ORANGE:
+                        tile.setFill(javafx.scene.paint.Color.ORANGE);
+                        break;
+                    case YELLOW:
+                        tile.setFill(javafx.scene.paint.Color.YELLOW);
+                        break;
+                    case GREEN:
+                        tile.setFill(javafx.scene.paint.Color.GREEN);
+                        break;
+                    case BLUE:
+                        tile.setFill(javafx.scene.paint.Color.BLUE);
+                        break;
+                    case PURPLE:
+                        tile.setFill(javafx.scene.paint.Color.PURPLE);
+                        break;
+                    case WHITE:
+                        tile.setFill(javafx.scene.paint.Color.WHITE);
+                        break;
+                
+                    default:
+                        tile.setFill(javafx.scene.paint.Color.GRAY);
+                        break;
+                }
+
+				tile.setStroke(javafx.scene.paint.Color.BLACK);
+
+				gameBoard.add(new StackPane(tile), j, i);
+
+			}
+        }
+
+        scene.setRoot(gameBoard);
     }
     
 }
